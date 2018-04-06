c5457bc848e071597e9194f8b3d49ad5062e9a7c
ksabry <ksabry@localhost.localdomain>
2018-04-06 16:17:01 -0600

    Fix TypePackElement

--- src/Util/TypePackElement.h
+++ src/Util/TypePackElement.h
@@ -7,7 +7,7 @@ namespace Ravel
 {
 	// See https://ldionne.com/2015/11/29/efficient-parameter-pack-indexing/
 
-	template <uint32_t I, typename T>
+	template <std::size_t I, typename T>
 	struct indexed {
 		using type = T;
 	};
@@ -15,15 +15,15 @@ namespace Ravel
 	template <typename Is, typename ...Ts>
 	struct indexer;
 
-	template <uint32_t ...Is, typename ...Ts>
+	template <std::size_t ...Is, typename ...Ts>
 	struct indexer<std::index_sequence<Is...>, Ts...>
 		: indexed<Is, Ts>...
 	{ };
 
-	template <uint32_t I, typename T>
+	template <std::size_t I, typename T>
 	static indexed<I, T> select(indexed<I, T>);
 
-	template <uint32_t I, typename ...Ts>
+	template <std::size_t I, typename ...Ts>
 	using type_pack_element = typename decltype(select<I>(
 		indexer<std::index_sequence_for<Ts...>, Ts...>{}
 	))::type;
